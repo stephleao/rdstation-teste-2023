b381f630fd3668fc6dcb041ee87e73295587ad2c
Stephanne Leão <stephanne.leao@edifyeducation.com.br>
2023-05-12 17:28:14 -0300

    HTML e CSS: Configuração inicial

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#ffffff">
+  <title>RD Station</title>
+  <meta name="description" content="Do Marketing às Vendas, a RD Station é a plataforma com tudo para sua empresa criar uma Máquina de Crescimento. Teste de graça!">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@800;900&family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="shortcut icon" href="/images/logo-icons/i-rdstation.svg">
+  <link rel="stylesheet" href="/css/main.css">
+</head>
+
+<body>
+  <a href="#main" class="visually-hidden-focusable">Saltar para o conteúdo</a>
+
+  <main id="main"></main>
+
+  <script type="module" src="/node_modules/ionicons/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="/node_modules/ionicons/dist/ionicons/ionicons.js"></script>
+  <script src="/js/main.bundle.js"></script>
+</body>
+
+</html>
