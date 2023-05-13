39d44ccca28d66eda0ece88007efbfefc1ce5a36
Stephanne Leão <stephanneleao@gmail.com>
2023-05-12 21:12:44 -0300

    HTML: Estrutura da seção Hero

--- index.php
+++ index.php
@@ -19,7 +19,10 @@
 
   <?php include 'template-parts/header.php'; ?>
 
-  <main id="main"></main>
+  <main id="main">
+    <?php include 'template-parts/section-hero.php'; ?>
+
+  </main>
 
   <script type="module" src="/node_modules/ionicons/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="/node_modules/ionicons/dist/ionicons/ionicons.js"></script>
