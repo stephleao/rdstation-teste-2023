1199f185f287a7debb74297d84f059ef6231eac1
Stephanne Leão <stephanneleao@gmail.com>
2023-05-16 12:32:04 -0300

    HTML e CSS: secao integrations finalizada

--- template-parts/section-integrations.php
+++ template-parts/section-integrations.php
@@ -48,27 +48,27 @@ $tools_list = array(
 );
 ?>
 
-<section class="section-integrations">
+<section class="section-integrations section-wrapper">
   <div class="container">
     <div class="row">
-      <div class="col">
-        <h1>Torne sua operação de Marketing ainda mais poderosa</h1>
-        <p>O RD Station Marketing se conecta as principais ferramentas nacionais e internacionais.</p>
+      <div class="col-12 col-lg-6 mb-4">
+        <h1 class="h2">Torne sua operação de Marketing ainda mais poderosa</h1>
+        <p class="lead">O RD Station Marketing se conecta as principais ferramentas nacionais e internacionais.</p>
       </div>
 
-      <div class="col">
-        <ul class="row">
+      <div class="col-12 col-lg-6">
+        <ul class="list-unstyled row row-cols-2 row-cols-md-5 g-3 text-center">
           <?php foreach ($tools_list as $tool) : ?>
             <li class="col">
               <img class="" src="/images/logo-icons/<?= $tool['logo']; ?>.svg" width="40" height="40" alt="<?= $tool['name']; ?>">
-              <p aria-hidden="true"><?= $tool['name']; ?></p>
+              <p class="small text-black mt-2" aria-hidden="true"><?= $tool['name']; ?></p>
             </li>
           <?php endforeach; ?>
         </ul>
+      </div>
 
-        <div class="col">
-          <a class="btn btn-primary" href="#lead-form">Criar conta gratuita</a>
-        </div>
+      <div class="col-12 text-center text-lg-start">
+        <a class="btn btn-primary" href="https://app.rdstation.com.br/signup" target="_blank" rel="noopener">Criar conta gratuita</a>
       </div>
     </div>
   </div>
