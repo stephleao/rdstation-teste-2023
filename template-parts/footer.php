f52c626198188d5184e6fa1361095240a78c5877
Stephanne Leão <stephanneleao@gmail.com>
2023-05-16 14:50:05 -0300

    HTML: Footer finalizado

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -36,20 +36,20 @@ $social_list = array(
 
 <footer class="footer">
   <div class="container">
-    <div class="row">
-      <div class="col">
+    <div class="row g-0 justify-content-between align-items-center">
+      <div class="col-12 col-sm-auto mb-3 mb-md-0">
         <a class="brand" href="/" rel="home">
-          <ion-icon class="brand" src="/images/rdstation-logo.svg" aria-hidden="true"></ion-icon>
+          <ion-icon class="brand-rdstation text-white" src="/images/rdstation-logo.svg" aria-hidden="true"></ion-icon>
           <span class="visually-hidden">RD Station</span>
         </a>
       </div>
-      <div class="col">
-        <ul class="social-menu">
+      <div class="col-12 col-sm-auto">
+        <ul class="social-menu nav justify-content-center justify-content-md-end">
           <?php foreach ($social_list as $social) :
             $ionicon_attr = $social['icon'] ? 'src="/images/logo-icons/' . $social['icon'] . '.svg"' : 'name="logo-' . $social['name'] . '"';
           ?>
-            <li class="">
-              <a target="_blank" rel="noopener" href="<?= $social['url']; ?>" class="">
+            <li class="nav-item">
+              <a target="_blank" rel="noopener" href="<?= $social['url']; ?>" class="nav-link">
                 <ion-icon <?= $ionicon_attr; ?> aria-hidden="true"></ion-icon>
                 <span class="visually-hidden"><?= $social['name']; ?></span>
               </a>
@@ -57,8 +57,8 @@ $social_list = array(
           <?php endforeach; ?>
         </ul>
       </div>
-      <div class="col">
-        <p>© 2021 RD Station. <a href="https://legal.rdstation.com/pt/privacy-policy/">Política de Privacidade</a>.</p>
+      <div class="col-12 copyright">
+        <p>© 2021 <strong>RD Station</strong>. <a href="https://legal.rdstation.com/pt/privacy-policy/">Política de Privacidade</a>.</p>
       </div>
     </div>
   </div>
