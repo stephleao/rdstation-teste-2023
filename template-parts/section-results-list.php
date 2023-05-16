567e0bb8ecdabbb69e4a488d1aee9a048156b7bd
Stephanne Leão <stephanneleao@gmail.com>
2023-05-16 15:30:37 -0300

    HTML e CSS: secao results finalizada

--- template-parts/section-results-list.php
+++ template-parts/section-results-list.php
@@ -3,26 +3,37 @@
 /**
  * Template-part: Section Results List
  */
+
+$content_list = array(
+  '<strong>Ferramentas intuitivas</strong> e com modelos prontos, vídeos explicativos e passo a passo guiado',
+  '<strong>Implementação com especialistas</strong> focada em planejamento e prática (nos planos Marketing Pro e Enterprise)',
+  '<strong>Time de especialistas em Sucesso do Cliente</strong> para apoiar na otimização da estratégia em cada fase da sua jornada (nos planos Marketing Pro e Enterprise)',
+  '<strong>Suporte técnico</strong> em português, espanhol e inglês para tirar dúvidas, <strong>com 97% de satisfação</strong> entre nossos clientes',
+  '<strong>Muito conteúdo educativo</strong>, eventos, cursos da <a href="https://rduniversity.com.br/" target="_blank" rel="noopener">RD University</a> e comunidades para você continuar evoluindo',
+);
 ?>
 
-<section class="section-results-list">
+<section class="section-results-list section-wrapper bg-light">
   <div class="container">
-    <div class="row">
-      <div class="col">
-        <h1>Resultados previsíveis e mais clientes para sua empresa</h1>
-        <p class="lead">Torne sua atuação no Marketing Digital mais eficaz</p>
+    <div class="row justify-content-between">
+      <div class="col-12 col-lg-5 mb-4 mb-lg-0">
+        <h1 class="h3">Resultados previsíveis e mais clientes para sua empresa</h1>
+        <p class="lead mb-4 pb-lg-3">Torne sua atuação no Marketing Digital mais eficaz</p>
 
-        <ul class="fancy-list">
-          <li><strong>Ferramentas intuitivas</strong> e com modelos prontos, vídeos explicativos e passo a passo guiado</li>
-          <li><strong>Implementação com especialistas</strong> focada em planejamento e prática (nos planos Marketing Pro e Enterprise)</li>
-          <li><strong>Time de especialistas em Sucesso do Cliente</strong> para apoiar na otimização da estratégia em cada fase da sua jornada (nos planos Marketing Pro e Enterprise)</li>
-          <li><strong>Suporte técnico</strong> em português, espanhol e inglês para tirar dúvidas, <strong>com 97% de satisfação</strong> entre nossos clientes</li>
-          <li><strong>Muito conteúdo educativo</strong>, eventos, cursos da <a href="">RD University</a> e comunidades para você continuar evoluindo</li>
+        <ul class="list-icon">
+          <?php foreach ($content_list as $content) : ?>
+            <li class="mb-3">
+              <span class="i-li">
+                <ion-icon class="text-primary" src="/images/icons/i-checkmark.svg" aria-hidden="true"></ion-icon>
+              </span>
+              <?= $content; ?>
+            </li>
+          <?php endforeach; ?>
         </ul>
       </div>
 
-      <div class="col">
-        <img src="/images/img-results.jpg" width="600" height="600" alt="Mulher escrevendo resultados no vidro">
+      <div class="col-12 col-lg-6 order-lg-first">
+        <img class="img-fluid" src="/images/img-results.jpg" width="600" height="600" alt="Mulher escrevendo resultados no vidro">
       </div>
     </div>
   </div>
