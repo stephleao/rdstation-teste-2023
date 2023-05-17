6b493dc1c18d92a02b3cad65d197f0e9325efe23
Stephanne Leão <stephanneleao@gmail.com>
2023-05-17 01:55:17 -0300

    HTML e JS: Header: Atualizacao com classes de BS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
+// Webpack Imports
+// import BS components
+import Collapse from 'bootstrap/js/dist/collapse';
+import Dropdown from 'bootstrap/js/dist/dropdown';
+
 (function () {
   "use strict";
 
--- template-parts/header.php
+++ template-parts/header.php
@@ -5,62 +5,67 @@
  */
 ?>
 
-<header class="header main-navbar">
-  <nav class="container">
+<header class="header-navbar navbar navbar-expand-lg">
+  <nav class="container-lg">
     <a class="brand" href="/" rel="home">
-      <ion-icon class="brand" src="/images/rdstation-logo.svg" aria-hidden="true"></ion-icon>
+      <ion-icon class="brand-rdstation text-primary" src="/images/rdstation-logo.svg" aria-hidden="true"></ion-icon>
       <span class="visually-hidden">RD Station</span>
     </a>
 
-    <button class="navbar-toggle" type="button" aria-controls="main-menu-wrapper" aria-expanded="false" aria-label="Abrir menu">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu-wrapper" aria-controls="main-menu-wrapper" aria-expanded="false" aria-label="Abrir menu">
       <ion-icon name="menu" aria-hidden="true"></ion-icon>
     </button>
 
-    <div id="main-menu-wrapper" class="">
-      <ul class="">
-        <li class="item">
-          <a href="#">Menu 1</a>
+    <div id="main-menu-wrapper" class="collapse navbar-collapse justify-content-end">
+      <div class="flex-fill" aria-hidden="true"></div>
+      <ul class="main-nav navbar-nav flex-fill justify-content-evenly">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu 1</a>
         </li>
-        <li class="item dropdown">
-          <button class="dropdown-toggle" type="button" aria-expanded="false">Menu 2</button>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
           <div class="dropdown-menu">
-            <div class="">
-              <div class="dropdown-header">Metodologia</div>
-              <ul>
-                <li><a class="dropdown-item" href="#">Tudo sobre Inbound Marketing</a></li>
-              </ul>
-            </div>
-            <div class="">
-              <div class="dropdown-header">Cursos</div>
-              <ul>
-                <li><a class="dropdown-item" href="#">RD University</a></li>
-                <li><a class="dropdown-item" href="#">Introdução ao Inbound Marketing</a></li>
-                <li><a class="dropdown-item" href="#">RD Station Marketing Basic</a></li>
-              </ul>
-            </div>
-            <div class="">
-              <div class="dropdown-header">Ferramentas</div>
-              <ul>
-                <li><a class="dropdown-item" href="#">Avalie seu funil de vendas</a></li>
-                <li><a class="dropdown-item" href="#">Compare suas métricas de Email Marketing</a></li>
-                <li><a class="dropdown-item" href="#">Calcule o ROI do RD Station Marketing</a></li>
-              </ul>
+            <div class="row">
+              <div class="col-12 col-lg-4">
+                <div class="dropdown-header">Metodologia</div>
+                <ul>
+                  <li><a class="dropdown-item" href="#">Tudo sobre Inbound Marketing</a></li>
+                </ul>
+              </div>
+              <div class="col-12 col-lg-4">
+                <div class="dropdown-header">Cursos</div>
+                <ul>
+                  <li><a class="dropdown-item" href="#">RD University</a></li>
+                  <li><a class="dropdown-item" href="#">Introdução ao Inbound Marketing</a></li>
+                  <li><a class="dropdown-item" href="#">RD Station Marketing Basic</a></li>
+                </ul>
+              </div>
+              <div class="col-12 col-lg-4">
+                <div class="dropdown-header">Ferramentas</div>
+                <ul>
+                  <li><a class="dropdown-item" href="#">Avalie seu funil de vendas</a></li>
+                  <li><a class="dropdown-item" href="#">Compare suas métricas de Email Marketing</a></li>
+                  <li><a class="dropdown-item" href="#">Calcule o ROI do RD Station Marketing</a></li>
+                </ul>
+              </div>
             </div>
           </div>
         </li>
-        <li class="item">
-          <a href="#">Menu 3</a>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu 3</a>
         </li>
-        <li class="item">
-          <a href="#">Menu 4</a>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu 4</a>
         </li>
-        <li class="item">
-          <a href="#">Menu 5</a>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu 5</a>
         </li>
       </ul>
 
-      <div class="">
-        <a class="btn btn-secondary" href="#">Botão</a>
+      <div class="secondary-nav">
+        <a class="btn btn-secondary me-3" href="#">Botão</a>
         <a class="btn btn-primary" href="#">Botão <ion-icon class="i-arrow-right" src="/images/icons/i-arrow-forward.svg" aria-hidden="true"></ion-icon></a>
       </div>
     </div>
