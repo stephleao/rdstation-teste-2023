5fdc1587b8d4d47c5c60bd012c0a1a79f97f804c
Stephanne Leão <stephanneleao@gmail.com>
2023-05-16 23:44:13 -0300

    HTML: secao demo finalizada

--- template-parts/section-demo.php
+++ template-parts/section-demo.php
@@ -5,18 +5,18 @@
  */
 ?>
 
-<section class="section-demo">
+<section class="section-demo section-wrapper section-graphics-02-left">
   <div class="container">
-    <div class="row">
-      <div class="col">
-        <h1>Entenda melhor como o RD Station Marketing funciona na prática</h1>
+    <div class="row align-items-center">
+      <div class="col-12 col-md-5 col-xl-4 mb-4 mb-md-0">
+        <h1 class="h3 pb-md-4">Entenda melhor como o RD Station Marketing funciona na prática</h1>
 
         <?php // Este botao deve acionar o modal para assistir ao video... ou...
         ?>
-        <a class="btn btn-secondary" href="#" aria-hidden="true">Assista à demonstração <ion-icon name="play-circle"></ion-icon></a>
+        <a class="btn btn-secondary" href="#" aria-hidden="true">Assista à demonstração <ion-icon class="i-arrow" name="play-circle"></ion-icon></a>
       </div>
 
-      <div class="col">
+      <div class="col-12 col-md-7 col-lg-6 offset-lg-1">
         <?php
         // ...quando clica no play em cima do video?
         // (que, no caso, fiz toda a thumb ser clicavel porque aumenta a area de clique do usuario)
@@ -25,10 +25,13 @@
         // o botao do leitor de tela.
         ?>
         <a class="thumb-cover" href="https://www.youtube.com/watch?v=lGCesaaLi4s" title="Clique para ver o vídeo">
-          <img src="/images/img-video-thumbnail.jpg" width="649" height="365" alt=" Prévia do vídeo">
+          <img class="thumb-cover-img" src="/images/img-video-thumbnail.jpg" width="649" height="365" alt=" Prévia do vídeo">
 
           <div class="thumb-cover-caption" aria-hidden="true">
-            <ion-icon class="i-play" name="play"></ion-icon>
+            <div class="icon-wrapper icon-size-3 icon-pulse">
+              <div class="i-backdrop"></div>
+              <ion-icon class="i-play" name="play"></ion-icon>
+            </div>
           </div>
         </a>
       </div>
